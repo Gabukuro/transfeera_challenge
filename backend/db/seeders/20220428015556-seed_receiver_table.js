62223032fbb0dc887469ad21540ce6ab82483972
Gabriel Machado <gabriel.machado@totvs.com.br>
2022-04-28 00:46:58 -0300

    if condition fixed

--- backend/db/seeders/20220428015556-seed_receiver_table.js
+++ backend/db/seeders/20220428015556-seed_receiver_table.js
@@ -3,15 +3,15 @@ var faker = require('faker-br');
 
 const receivers = [...Array(30)].map((receiver) => {
   let cpf = faker.br.cpf();
-  let shouldBeDraft = Math.floor((Math.random() * 50) + 1) % 2 == 0;
+  let shouldBeDraft = Math.floor((Math.random() * 50) + 1) % 2 !== 0;
 
   return {
     name: faker.name.findName(),
     email: faker.internet.email(),
-    cpfCnpj: shouldBeDraft ? cpf : null,
+    cpfCnpj: !shouldBeDraft ? cpf : null,
     pixType: 'CPF',
     pixKey: cpf,
-    status: shouldBeDraft ? 'valid' : 'draft'
+    status: !shouldBeDraft ? 'valid' : 'draft'
   };
 })
 
