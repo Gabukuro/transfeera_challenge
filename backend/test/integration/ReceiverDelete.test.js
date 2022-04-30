a3eb97ff51136f73429e0abb38fc8458fbc32f21
Gabriel Machado <gabriel.machado@totvs.com.br>
2022-04-29 22:36:27 -0300

    tests of delete and paginate receivers writed

---
+++ backend/test/integration/ReceiverDelete.test.js
@@ -0,0 +1,33 @@
+const app = require('../../src/app');
+const request = require('supertest');
+const ReceiverModel = require('../../db/models').Receiver;
+
+require('../TestSetup');
+
+const ReceiverTestSetup = require('../../test/integration/ReceiverTestSetup');
+
+describe('delete receiver tests', () => {
+
+    it('shoul delete a receiver successfully', async () => {
+        ReceiverTestSetup.createDummyReceiver();
+
+        const response = await request(app)
+            .delete(`/receiver/1`)
+            .expect(200);
+
+        expect(response.body.message).toEqual('Receiver deleted successfully');
+        expect(await ReceiverModel.count()).toEqual(0);
+    });
+
+    it('should delete multiple receivers successfully', async () => {
+        ReceiverTestSetup.createDummyReceivers(10);
+
+        const response = await request(app)
+            .post(`/receiver/delete-request`)
+            .send([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+            .expect(200);
+
+        expect(response.body.message).toEqual('Receivers deleted successfully');
+        expect(await ReceiverModel.count()).toEqual(0);
+    });
+});
--- backend/test/integration/ReceiverPaginate.test.js
+++ backend/test/integration/ReceiverPaginate.test.js
@@ -3,16 +3,40 @@ const request = require('supertest');
 
 require('../TestSetup');
 
+const ReceiverTestSetup = require('../../test/integration/ReceiverTestSetup');
+const { sequelize } = require('../../db/models');
+beforeAll(async () => ReceiverTestSetup.createDummyReceivers(30));
+
 describe('paginate receivers test', () => {
-    describe('shoul paginate receivers', () => {
+    describe('should paginate receivers', () => {
         test.each([1, 2, 3])('pageNumber: %s', async pageNumber => {
             const response = await request(app)
-                .get(`/receiver?page=${pageNumber}&limit=5`)
+                .get(`/receiver?page=${pageNumber}&limit=10`)
                 .expect(200);
 
             let body = response.body;
 
-            expect(body.length).toBeLessThanOrEqual(5);
+            expect(body.data.length).toBeLessThanOrEqual(10);
+        });
+    });
+
+    describe('should filter receivers', () => {
+        test.each([
+            { key: 'pix_key', value: '82939397023' },
+            { key: 'pix_key', value: '+5511999999999' },
+            { key: 'pix_key', value: 'emaiu@email.com' },
+            { key: 'email', value: 'email2@email.com' },
+            { key: 'name', value: 'Benjamin Kirby Tennyson' },
+            { key: 'status', value: 'valid' }
+        ])('filter: %s', async filter => {
+            const response = await request(app)
+                .get(`/receiver?page=1&limit=10&filter=${filter.value}`)
+                .expect(200);
+
+            let body = response.body;
+
+            let receiversCount = await sequelize.quey(`SELECT COUNT(${filter.key}) FROM receivers WHERE ${filter.key} = '${filter.value}'`);
+            expect(body.data.length).toBeLessThanOrEqual(receiversCount);
         });
     });
 });
