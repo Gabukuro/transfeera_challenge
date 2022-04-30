606c062c9fa492960f9f94247f82214d8b09ba1f
Gabriel Machado <gabriel.machado@totvs.com.br>
2022-04-30 01:50:46 -0300

    fixing stuck jest

--- backend/test/TestSetup.js
+++ backend/test/TestSetup.js
@@ -3,3 +3,7 @@ const models = require('../db/models');
 beforeEach(async () => {
     await models.sequelize.sync({ force: true })
 });
+
+afterAll(async () => {
+    await models.sequelize.close();
+});
