78f3282303bb30b54a35aaf916a7f395435b3026
Gabriel Machado <gabriel.machado@totvs.com.br>
2022-04-30 01:50:56 -0300

    refatoring test

--- backend/test/integration/ReceiverCreate.test.js
+++ backend/test/integration/ReceiverCreate.test.js
@@ -8,23 +8,13 @@ const dummyReceiver = {
     name: faker.name.findName(),
     email: faker.internet.email(),
     cpf_cnpj: faker.br.cpf(),
+    pix_key_type: 'CPF',
+    pix_key: faker.br.cpf()
 }
-
-const pixKeys = [
-    { key_type: 'CPF', key: faker.br.cpf() },
-    { key_type: 'CNPJ', key: faker.br.cnpj() },
-    { key_type: 'EMAIL', key: faker.internet.email() },
-    { key_type: 'TELEFONE', key: `+5511999999999` },
-    { key_type: 'CHAVE_ALEATORIA', key: `a234567d-a12a-a12a-a12a-aa123456789a` },
-]
 
 describe('create receiver tests', () => {
     it('should create a valid receiver', async () => {
-        let receiver = { 
-            ...dummyReceiver, 
-            pix_key_type: pixKeys[0].key_type, 
-            pix_key: pixKeys[0].key 
-        };
+        let receiver = dummyReceiver;
 
         const response = await request(app)
             .post('/receiver')
@@ -39,11 +29,7 @@ describe('create receiver tests', () => {
 
     it('should throw an error when receiver is invalid', async () => {
 
-        let receiver = { 
-            ...dummyReceiver, 
-            pix_key_type: pixKeys[0].key_type, 
-            pix_key: 'abublé' 
-        };
+        let receiver = { ...dummyReceiver, pix_key: 'abublé' };
 
         const response = await request(app)
             .post('/receiver')
