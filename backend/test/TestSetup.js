1974b4d95af9c1a26fdc7fd0c5747c16ae0ea531
Gabriel Machado <gabriel.machado@totvs.com.br>
2022-04-30 01:30:48 -0300

    fixing handle that was keeping jest from exiting

--- backend/test/TestSetup.js
+++ backend/test/TestSetup.js
@@ -3,8 +3,3 @@ const models = require('../db/models');
 beforeEach(async () => {
     await models.sequelize.sync({ force: true })
 });
-
-afterAll(async () => {
-    await models.sequelize.close();
-    await new Promise(resolve => setTimeout(() => resolve(), 500));
-});
